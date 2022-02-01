67b34151390906856a9736684ec8a97bc27f385c
aheisleycook <aheisleycook@scienceandtechnologysociety.attlocal.net>
2022-02-01 14:00:03 -0800

    controller and views

---
+++ controller/controller.php
@@ -0,0 +1,19 @@
+<?php
+require("views.php");
+class Controller {
+
+	/**
+	 */
+	function __construct() {
+        $view = new View();
+	}
+    function get_view() {
+        $url = $_GET;
+        if($url == "/") {
+            $view->set_
+        }
+        
+        
+    }
+}
+ 
---
+++ views/views.php
@@ -0,0 +1,10 @@
+<?php
+class Views
+{
+public function __construct() {
+ 
+}
+public function Home() {
+    header('Location:"/views/home/');
+}
+}

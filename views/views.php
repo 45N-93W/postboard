c938325ee0fe343bbd5d86a414d2c0dafa8f5c39
atheprogrammer <atheprogrammer@protonmail.com>
2022-02-04 09:24:29 -0800

    added

--- views/views.php
+++ views/views.php
@@ -1,10 +1,19 @@
 <?php
 class Views
 {
-public function __construct() {
- 
-}
-public function Home() {
-    header('Location:"/views/home/');
-}
+    public function __construct()
+    {
+    }
+    public function Home()
+    {
+        header('Location:"/views/home/');
+    }
+    public function About()
+    {
+        header('Location:"/views/about/');
+    }
+    public function login()
+    {
+        header('Location:"/views/login/');
+    }
 }
